0d063f2034cfd7043c55f0af7c0c0ff696f97307
Alex4790354 <9720560@gmail.com>
2022-05-06 23:34:44 +0300

    Add first microservice

---
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsApplication.java
@@ -0,0 +1,14 @@
+package com.appsdeveloperblog.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MobileAppWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppWsApplication.class, args);
+	}
+}
